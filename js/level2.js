7a9df20d73f7f135d3757e40c9bd2e819127ee4e
NicoGovStudent <69640301+NicoGovStudent@users.noreply.github.com>
2021-05-27 00:48:45 +0200

    made level 2 bigger

--- js/level2.js
+++ js/level2.js
@@ -16,32 +16,20 @@ const dirLight2 = new THREE.DirectionalLight(0xffffff, 0.6);
 dirLight2.position.set(-500, 400, 100);
 //scene.add(dirLight2);
 
-//View settings - pick which camera/////////////////////////////////////////////
-var play = true;
-
-let viewBtn = document.querySelector("#buttonView");
-viewBtn.addEventListener('click', ()=>{
-    play = false;
-});
-
-let playBtn = document.querySelector("#buttonPlay");
-playBtn.addEventListener('click', ()=>{
-    play = true;
-});
+//////////////////////View settings - pick which camera////////////////////////////
+var play = true; //MANUAL CHOICE!
 
 const aspectRatio = window.innerWidth / window.innerHeight;
 const cameraWidth = 3500;
 const cameraHeight = cameraWidth / aspectRatio;
 var camera;
-
 setCamera(play);
-
 
 ///////////////////////////////////////////////////////////////////////////////////
 
 const room = Room();
 //room.scale.setScalar(1);
-room.position.set(900,-300,0);
+//room.position.set(900,-300,0);
 scene.add(room);
 
 //skybox
@@ -82,8 +70,8 @@ function setCamera(isPlay) {
             100000 //far plane
         );
 
-        camera.position.set(1000,50,200);
-        camera.lookAt(1300,0,2000)
+        camera.position.set(0,300,600);
+        camera.lookAt(-300,100,-600);
     } else {
         //setting up orthographic camera
             camera = new THREE.OrthographicCamera(
@@ -91,13 +79,13 @@ function setCamera(isPlay) {
             cameraWidth / 2, //right
             cameraHeight / 2, //top
             cameraHeight / -2, //bottom
-            -800, //near
-            5000 //far
+            -2000, //near
+            10000 //far
         );
 
-        camera.position.set(-300, 200, -300);
+        camera.position.set(0, 1000, -1500);
         camera.up.set(0, 1, 0);
-        camera.lookAt(0, 0, 0);
+        camera.lookAt(0, 0, -1500);
     }
 }
 
@@ -118,7 +106,6 @@ function processKeyboard(){
 }
 
 function drawScene(){
-    //setCamera(play);
     renderer.render(scene, camera);
     processKeyboard();
     requestAnimationFrame(drawScene);
@@ -181,17 +168,17 @@ function RoomPart() {
     //FLOORS
         //main floor 1 - f1
         const f1 = Floor()
-        f1.scale.set(4,1,8);
+        f1.scale.set(5,1,9);
         roomPart.add(f1);
 
         //floors 2 and 3
         const f2 = Floor();
-        f2.scale.set(2,1,2);
+        f2.scale.set(3,1,3);
         f2.position.set(600,0,600);
         roomPart.add(f2);
 
         const f3 = Floor();
-        f3.scale.set(2,1,2);
+        f3.scale.set(3,1,3);
         f3.position.set(600,0,-600);
         roomPart.add(f3);
 
@@ -199,29 +186,29 @@ function RoomPart() {
         const w2 = Wall();
         w2.scale.set(5,1,1);
         w2.rotateY(Math.PI/2);
-        w2.position.set(820,200,550);
+        w2.position.set(820,200,600);
         roomPart.add(w2);
 
         const w6 = Wall();
         w6.scale.set(5,1,1);
         w6.rotateY(Math.PI/2);
-        w6.position.set(820,200,-550);
+        w6.position.set(820,200,-600);
         roomPart.add(w6);
 
         const w5 = Wall();
         w5.scale.set(5,1,1);
-        w5.position.set(600,200,-380);
+        w5.position.set(725,200,-325);
         roomPart.add(w5);
 
         const w3 = Wall();
         w3.scale.set(5,1,1);
-        w3.position.set(600,200,380);
+        w3.position.set(725,200,325);
         roomPart.add(w3);
 
         const w4 = Wall();
-        w4.scale.set(8,1,1);
+        w4.scale.set(7,1,1);
         w4.rotateY(Math.PI/2);
-        w4.position.set(375,200,0);
+        w4.position.set(500,200,0);
         roomPart.add(w4);
 
     return roomPart;
@@ -236,43 +223,38 @@ function Room() {
         room.add(roomPart1);
 
         const roomPart2 = RoomPart();
-        roomPart2.position.set(320,0,-1600);
+        roomPart2.position.set(320,0,-1650);
         roomPart2.rotateY(Math.PI);
         room.add(roomPart2);
 
         const roomPart3 = RoomPart();
-        roomPart3.position.set(400,0,-1600);
+        roomPart3.position.set(400,0,-1650);
         room.add(roomPart3);
 
     //FLOORS
         //floors 4 and 5
         const f4 = Floor();
-        f4.scale.set(4,1,5);
-        f4.position.set(-800,0,300);
+        f4.scale.set(6,1,8);
+        f4.position.set(-1100,0,0);
         room.add(f4);
-
-        const f5 = Floor();
-        f5.scale.set(4,1,3);
-        f5.position.set(-800,0,-500);
-        room.add(f5);
     
     //WALLS
         //walls 1,7
         const w1 = Wall();
-        w1.scale.set(21,1,1);
-        w1.position.set(-210,200,825);
+        w1.scale.set(25,1,1);
+        w1.position.set(-450,200,825);
         room.add(w1);
 
         const w7 = Wall();
-        w7.scale.set(15,1,1);
-        w7.position.set(-450,200,-800);
+        w7.scale.set(12,1,1);
+        w7.position.set(-1100,200,-825);
         room.add(w7);
 
         //walls 10,13,15,3r
         const w10 = Wall();
         w10.scale.set(10,1,1);
         w10.rotateY(Math.PI/2);
-        w10.position.set(-1200,200,350);
+        w10.position.set(-1700,200,350);
         room.add(w10);
 
         const w13 = Wall();
@@ -288,15 +270,15 @@ function Room() {
         room.add(w15);
 
         const w3r = Wall();
-        w3r.scale.set(5,1,1);
-        w3r.position.set(1050,200,-780);
+        w3r.scale.set(4,1,1);
+        w3r.position.set(1035,200,-825);
         room.add(w3r);
 
         //walls with windows
         const w9 = WindowedWall();
         w9.scale.set(0.8,1,1);
         w9.rotateY(Math.PI/2);
-        w9.position.set(-1200,200,-450);
+        w9.position.set(-1700,200,-450);
         room.add(w9);
 
         const w14 = WindowedWall();
@@ -314,18 +296,18 @@ function Room() {
         //end of level wall
         const w20 = Wall();
         w20.scale.set(6,1,1);
-        w20.position.set(-200,200,-2400);
+        w20.position.set(-225,200,-2500);
         room.add(w20);
 
         const w21 = Wall();
         w21.scale.set(6,1,1);
-        w21.position.set(900,200,-2400);
+        w21.position.set(945,200,-2500);
         room.add(w21);
 
         const w21r = Wall();
-        w21r.scale.set(2,1,1);
+        w21r.scale.set(2,0.8,1);
         w21r.rotateZ(Math.PI/2);
-        w21r.position.set(200,500,-2400);
+        w21r.position.set(350,500,-2500);
         room.add(w21r);
 
     return room;
