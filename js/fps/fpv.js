645953e621724a10d01ec053ad8dabdf43c334df
TristanBookhan <65284370+TristanBookhan@users.noreply.github.com>
2021-05-28 22:39:02 +0200

    adding models to game code

--- js/fps/fpv.js
+++ js/fps/fpv.js
@@ -5,9 +5,9 @@ var renderer = new THREE.WebGLRenderer({antialias: true});
 scene.background = new THREE.Color(0xfafafa);
 renderer.setSize(innerWidth, innerHeight);
 cam.position.z = 5;
-cam.position.y = 0;
+cam.position.y = 2;
 document.body.appendChild(renderer.domElement);
-var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 100);
+var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
 directionalLight.position.set(0, 1, 0);
 directionalLight.castShadow = true;
 scene.add(directionalLight);
@@ -18,10 +18,16 @@ let grid = new THREE.GridHelper(100, 20, 0x0a0a0a, 0x0a0a0a);
 grid.position.set(0, -0.5, 0);
 scene.add(grid);
 
-let bGeo = new THREE.BoxGeometry(1, 1, 1);
-let bMat = new THREE.MeshStandardMaterial({color: 0x00ff00, wireframe: false});
-let cube = new THREE.Mesh(bGeo, bMat);
-scene.add(cube);
+// let bGeo = new THREE.BoxGeometry(1, 1, 1);
+// let bMat = new THREE.MeshStandardMaterial({color: 0x00ff00, wireframe: false});
+// let cube = new THREE.Mesh(bGeo, bMat);
+// scene.add(cube);
+
+new THREE.GLTFLoader().load('Blender Models/Level 2/Shelf/Shelf.gltf' , result => {
+    model = result.scene;
+    scene.add(model);
+    drawScene();
+});
 
 let controls = new THREE.PointerLockControls(cam, renderer.domElement);
 let clock = new THREE.Clock();
