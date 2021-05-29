98ace03db534a07171fb51e806a7cce31ebf89a2
TristanBookhan <65284370+TristanBookhan@users.noreply.github.com>
2021-05-29 22:23:03 +0200

    popUp general code

--- js/level1.js
+++ js/level1.js
@@ -2,62 +2,72 @@ const scene = new THREE.Scene();
 intensity = 1;
 len = 10;
 
-var _euler = new THREE.Euler( 0, 0, 0, 'YXZ' );
-
-var cam = new THREE.PerspectiveCamera(45, innerWidth/innerHeight, 1, 100000);
-var renderer = new THREE.WebGL1Renderer({antialias: true});
+var cam = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 1, 100000);
+var renderer = new THREE.WebGL1Renderer({ antialias: true });
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 renderer.setSize(innerWidth, innerHeight);
-cam.position.set(2100,50,200);
-cam.lookAt(2900,0,2000);
+cam.position.set(2100, 50, 200);
+cam.lookAt(2900, 0, 2000);
 
 var ambientLight = new THREE.AmbientLight(0xffffff, 0.05);//0.05
 scene.add(ambientLight);
 
 //back right room
-const light9 = new THREE.PointLight( 0xffffff, intensity, 2200 ,2 ); 
-light9.position.set( -100,375,2100 ); 
+const light9 = new THREE.PointLight(0xffffff, intensity, 2200, 2);
+light9.position.set(-100, 375, 2100);
 light9.castShadow = true;
-scene.add( light9 );
+scene.add(light9);
 
 
 //front right room
-const light7 = new THREE.PointLight( 0xffffff, intensity, 2500 ,2 ); 
-light7.position.set( -100,375,900 ); 
+const light7 = new THREE.PointLight(0xffffff, intensity, 2500, 2);
+light7.position.set(-100, 375, 900);
 light7.castShadow = true;
-scene.add( light7 );
+scene.add(light7);
 
 //passage light
-const light5 = new THREE.PointLight( 0xffffff, intensity, 800 ,2 ); 
-light5.position.set( 1300,375,1500 ); 
+const light5 = new THREE.PointLight(0xffffff, intensity, 800, 2);
+light5.position.set(1300, 375, 1500);
 light5.castShadow = true;
-scene.add( light5 );
+scene.add(light5);
 
 //emergency light
-const light3 = new THREE.PointLight( 0xff0000, 3, 900 ,2 ); 
-light3.position.set( 2000,200,1200 ); 
+const light3 = new THREE.PointLight(0xff0000, 3, 900, 2);
+light3.position.set(2000, 200, 1200);
 light3.castShadow = true;
-scene.add( light3 );
+scene.add(light3);
 
 //flourescent light
-const light1 = new THREE.PointLight( 0xffffff, intensity, 6000 ,2 ); 
-light1.position.set( 2500,375,1500 ); 
+const light1 = new THREE.PointLight(0xffffff, intensity, 6000, 2);
+light1.position.set(2500, 375, 1500);
 light1.castShadow = true;
 light1.shadow.bias = -0.0001;
-scene.add( light1 );
+scene.add(light1);
 
 
 const room = Room();
-room.scale.set(4,4,2.5);
-room.rotateX(3*Math.PI/2);
+room.scale.set(4, 4, 2.5);
+room.rotateX(3 * Math.PI / 2);
 scene.add(room);
 
 
 const box = sky();
 box.translateY(14600);
 scene.add(box);
+
+// var reticle = new THREE.Mesh(
+//     new THREE.RingBufferGeometry(15, 20, 34),
+//     new THREE.MeshBasicMaterial({ color: 0xffffff })
+// );
+// reticle.scale.set(1,0.2,1);
+// reticle.position.z = -70;
+// //reticle.position.x = 20;
+// reticle.lookAt(cam.position)
+// cam.add(reticle);
+// scene.add(cam);
+scene.updateMatrixWorld(true);
 
 document.body.appendChild(renderer.domElement);
 
@@ -80,62 +90,71 @@ let controls = new THREE.PointerLockControls(cam, renderer.domElement);
 let clock = new THREE.Clock();
 
 let btn1 = document.querySelector("#button1");
-btn1.addEventListener('clicAk', ()=>{
+btn1.addEventListener('clicAk', () => {
     controls.lock();
 });
 
 let keyboard = [];
-addEventListener('keydown', (e)=>{
+addEventListener('keydown', (e) => {
     keyboard[e.key] = true;
 });
-addEventListener('keyup', (e)=>{
+addEventListener('keyup', (e) => {
     keyboard[e.key] = false;
 });
-function processKeyboard(){
+function processKeyboard() {
     var speed = 15
-    if (keyboard['w']){
+    if (keyboard['w']) {
         controls.moveForward(speed);
     }
-    else if(keyboard['a']){
+    else if (keyboard['a']) {
         controls.moveRight(-speed);
     }
-    else if(keyboard['s']){
+    else if (keyboard['s']) {
         controls.moveForward(-speed);
     }
-    else if(keyboard['d']){
+    else if (keyboard['d']) {
         controls.moveRight(speed);
     }
-    else if(keyboard['r']){
+    else if (keyboard['r']) {
         controls.lock();
     }
 }
 
-function drawScene(){
-    if (len == 0){
+function drawScene() {
+    if (len == 0) {
         len = Math.floor(Math.random() * 10);
         ran = Math.floor(Math.random() * 6);
-        if (ran == 2){
+        if (ran == 2) {
             intensity = 0.3;
             light1.intensity = intensity;
             light5.intensity = intensity;
             light7.intensity = intensity;
             light9.intensity = intensity;
-        }else{
+        } else {
             intensity = 1;
             light1.intensity = intensity;
             light5.intensity = intensity;
             light7.intensity = intensity;
             light9.intensity = intensity;
         }
-    }else{
-        len = len-1;
-    }    
+    } else {
+        len = len - 1;
+    }
+
     
     renderer.render(scene, cam);
     processKeyboard();
     requestAnimationFrame(drawScene);
 }
 
+
 //window.addEventListener( 'mousemove', onMouseMove, false );
 
 drawScene();
+
+// const finder = new THREE.Mesh(
+//     new THREE.BoxBufferGeometry(200,200,200),
+//     new THREE.MeshLambertMaterial({color: 0xffffff})
+// );
+// finder.position.set(2500,25,1500);
+// scene.add(finder);
